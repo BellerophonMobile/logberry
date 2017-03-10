f80d26d0bca420ebf6d8da5d73b15e68ec067b32
Joe Kopena <tjkopena@bellerophonmobile.com>
2017-03-10 01:32:17 -0500

    fmt

--- Task_test.go
+++ Task_test.go
@@ -1,24 +1,24 @@
 package logberry
 
 import (
-	"testing"
 	"github.com/stretchr/testify/require"
+	"testing"
 )
 
 func Test_Failure(t *testing.T) {
 	require := require.New(t)
 
 	key := "status"
-	
+
 	err := Main.Failure("Epic societal collapse", D{key: 404})
 
 	t.Log("D", err.Data)
 	d, ok := err.Data[key]
 	require.True(ok)
-	
+
 	num, ok := d.(EventDataInt64)
 	require.True(ok)
 
 	require.Equal(num, EventDataInt64(404))
-	
+
 }
