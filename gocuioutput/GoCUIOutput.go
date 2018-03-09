17b8e286d0005926a1d2e5fb7beb5ecd08042fa5
Joe Kopena <tjkopena@bellerophonmobile.com>
2018-03-09 06:06:26 -0500

    GoCUI output driver.

---
+++ gocuioutput/GoCUIOutput.go
@@ -0,0 +1,43 @@
+package gocuioutput
+
+import (
+	"github.com/BellerophonMobile/logberry"
+	"github.com/jroimartin/gocui"
+)
+
+type GoCUIOutput struct {
+	root   *logberry.Root
+	gui    *gocui.Gui
+	output logberry.OutputDriver
+}
+
+func New(g *gocui.Gui, output logberry.OutputDriver) *GoCUIOutput {
+	return &GoCUIOutput{
+		gui: g,
+		output: output,
+	}
+}
+
+// Attach notifies the OutputDriver of its Root.  It should only be
+// called by a Root.
+func (o *GoCUIOutput) Attach(root *logberry.Root) {
+	o.root = root
+}
+
+// Detach notifies the OutputDriver that it has been removed from its
+// Root.  It should only be called by a root.
+func (o *GoCUIOutput) Detach() {
+	o.root = nil
+}
+
+// Event outputs a generated log entry, as called by a Root or a
+// chaining OutputDriver.
+func (o *GoCUIOutput) Event(event *logberry.Event) {
+
+	o.gui.Update(func(g *gocui.Gui) error {
+		o.output.Event(event)
+		return nil
+	})
+
+	// end Event
+}
